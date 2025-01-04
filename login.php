d0e2d0d88a866c33d4ce49696d6b065608c7751e
oddybagusifn <touroroy26@gmail.com>
2025-01-04 15:22:52 +0700

    ubah judul pada form login

--- login.php
+++ login.php
@@ -14,7 +14,7 @@
     <!-- navbar -->
     <nav class="navbar navbar-dark navbar-expand-lg bg-dark sticky-top">
         <div class="container">
-          <a class="navbar-brand fw-bold" href="index.php">My Daily Journal</a>
+          <a class="navbar-brand fw-bold" href="index.php">Welcome to My Daily Journal</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
